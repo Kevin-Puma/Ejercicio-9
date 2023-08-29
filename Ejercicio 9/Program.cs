146aff196da036c8989d5dbb5d8232ed4d7a6c6c
User <User@DESKTOP-CRJD420>
2023-08-28 23:27:19 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio 9/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Ingrese el primer número:");
+int numero1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el segundo número:");
+int numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el tercer número:");
+int numero3 = int.Parse(Console.ReadLine());
+
+int mayor = numero1;
+
+if (numero2 > mayor)
+{
+    mayor = numero2;
+}
+
+if (numero3 > mayor)
+{
+    mayor = numero3;
+}
+
+Console.WriteLine("El mayor de los tres números es: " + mayor + "");
